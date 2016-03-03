c758802b6f7df5b05318d8fc0962ab8f4e595cb9
Louis Pilfold <louis@lpil.uk>
2016-03-03 16:43:25 +0000

    It works!

--- src/sonic_pi_pipe.go
+++ src/sonic_pi_pipe.go
@@ -6,15 +6,14 @@ import (
 	"os"
 )
 
-const RUN_ADDRESS = "/run-code"
-const STOP_ADDRESS = "/stop-all-jobs"
-const SERVER = "localhost"
-const PORT = 4557
+// const RUN_ADDRESS = "/run-code"
+// const STOP_ADDRESS = "/stop-all-jobs"
 
 func main() {
 	data, _ := ioutil.ReadAll(os.Stdin)
 	pi_code := string(data)
-	message := osc.NewMessage(RUN_ADDRESS)
+	message := osc.NewMessage("/run-code")
+	message.Append("SONIC_PI_SEND")
 	message.Append(pi_code)
-	osc.NewClient(SERVER, PORT).Send(message)
+	osc.NewClient("localhost", 4557).Send(message)
 }
