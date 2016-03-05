ba9f9260ad0fa53d4d0e78151bcbe0fd5a8cf38b
Louis Pilfold <louis@lpil.uk>
2016-03-05 14:10:43 +0000

    Check CLI args

--- cmd/sonic-pi-pipe/main.go
+++ cmd/sonic-pi-pipe/main.go
@@ -4,17 +4,33 @@ import (
 	"github.com/hypebeast/go-osc/osc"
 	"io/ioutil"
 	"os"
+	"fmt"
 )
 
+const usage = `sonic-pi-pipe commands:
+	[No arguments]: Send piped code to the Sonic Pi server
+`
+
 func main() {
-	data, _ := ioutil.ReadAll(os.Stdin)
-	pi_code := string(data)
-	send(pi_code)
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		pipe_to_sonic_pi()
+		os.Exit(0)
+	}
+
+	switch args[0] {
+	default:
+		fmt.Print(usage)
+		os.Exit(1)
+	}
 }
 
-func send(code string) {
+func pipe_to_sonic_pi() {
+	data, _ := ioutil.ReadAll(os.Stdin)
+	pi_code := string(data)
 	message := osc.NewMessage("/run-code")
 	message.Append("SONIC_PI_PIPE")
-	message.Append(code)
+	message.Append(pi_code)
 	osc.NewClient("localhost", 4557).Send(message)
 }
