e3808af8e401238af48bb9f112a7ed1ea0881716
Louis Pilfold <louis@lpil.uk>
2016-03-06 12:29:19 +0000

    Colour MultiMessages

--- log/multi_message.go
+++ log/multi_message.go
@@ -2,13 +2,14 @@ package log
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 )
 
 func MultiMessage(arguments []interface{}) string {
 	run := arguments[0]
 	time := arguments[2]
-	info := arguments[5]
-	return fmt.Sprintf(
-		"\n[Run %d, Time %s]\n └ %s\n", run, time, info,
-	)
+	info := arguments[5].(string)
+	line1 := fmt.Sprintf("\n[Run %d, Time %s]\n", run, time)
+	line2 := fmt.Sprintf(" └ %s\n", color.MagentaString(info))
+	return line1 + line2
 }
