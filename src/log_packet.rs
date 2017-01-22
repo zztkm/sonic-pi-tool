250cc52ca535d67bea6be9f45f21bbdab9409dc2
Louis Pilfold <louis@lpil.uk>
2017-01-22 13:06:49 +0000

    Safer to_log_string

--- src/log_packet.rs
+++ src/log_packet.rs
@@ -4,23 +4,25 @@ pub fn to_log_string(packet: OscPacket) -> String {
     match packet {
         OscPacket::Message(msg) => {
             println!("OSC address: {}", msg.addr);
-            match msg.addr.as_ref() {
+            let log = match msg.addr.as_ref() {
                 "/log/info" => format_log_info(msg),
                 "/info" => format_log_info(msg),
-                _ => "".to_string(),
-            }
+                _ => None,
+            };
+            log.unwrap_or("".to_string())
         }
-        OscPacket::Bundle(bundle) => format!("Expected message {:?}", bundle),
+        OscPacket::Bundle(_bundle) => "".to_string(),
     }
 }
 
-fn format_log_info(msg: OscMessage) -> String {
-    let args = msg.args.unwrap();
-    if let &OscType::String(ref string) = args.get(1).unwrap() {
-        format!("=> \"{}\"\n", string)
-    } else {
-        "".to_string()
-    }
+fn format_log_info(msg: OscMessage) -> Option<String> {
+    msg.args
+        .as_ref()
+        .and_then(|args| args.get(1))
+        .and_then(|e| match e {
+            &OscType::String(ref string) => Some(format!("=> \"{}\"\n", string)),
+            _ => None,
+        })
 }
 
 
