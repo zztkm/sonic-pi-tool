d2be154c6c326d7404d5c05f8e695ed9582b42f3
mttpgn <matthew.a.pagan@gmail.com>
2017-07-30 08:02:59 -0500

    Added default location on Raspbian.

--- src/lib.rs
+++ src/lib.rs
@@ -94,6 +94,7 @@ pub fn logs() {
 pub fn start_server() {
     let paths = ["/Applications/Sonic Pi.app/server/bin/sonic-pi-server.rb",
                  "./app/server/bin/sonic-pi-server.rb",
+                 "/opt/sonic-pi/app/server/bin/sonic-pi-server.rb",
                  "/usr/lib/sonic-pi/server/bin/sonic-pi-server.rb"];
 
     match paths.iter().find(|&&p| Path::new(p).exists()) {
